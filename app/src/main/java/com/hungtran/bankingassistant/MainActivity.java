a8b75c34e80200bd79dd1def2a650fd8e2333f66
hungMta <hung250596@gmail.com>
2019-02-13 08:35:54 +0700

    init project

---
+++ app/src/main/java/com/hungtran/bankingassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hungtran.bankingassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
